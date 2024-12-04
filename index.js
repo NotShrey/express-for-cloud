65afbe819ffc0603cbb479bc4ae2f78eb3e372a8
shreyas <shreyas.nayak.003@gmail.com>
2024-12-04 11:09:28 +0530

    Intial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT ?? 3000
+
+app.get('/',(req,res)=>{
+    return res.json({message: "this is working fine"})
+})
+
+app.listen(PORT,()=> console.log(`server is running in ${PORT}`))
